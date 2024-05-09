f37abb0c0e389c7cef3ee96e58d366550fa2d7b8
Earnie the Coder <ek.business@yahoo.com>
2024-05-09 16:17:35 -0700

    Task 8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # Matches above cases with more t's
 
-puts ARGV[0].scan(/\[from: (.*?)\] \[to: (.*?)\] \[flags: (.*?)\]/).join(",")
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
